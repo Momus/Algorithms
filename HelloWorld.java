5096331d52e2ab87b6f353e1c4455db1f53dcfad
Dmitri G. Brengauz <dmitri@momus.net>
2016-12-13 21:10:24 -0700

    ¡ Hola Mundo ! works

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/** 
+
+This is a little program to test my environment.
+
+*/
+
+//import algs4
+
+public class HelloWorld {
+    public static void main(String[] args) {
+	System.out.print("¡ Hola Mundo !\n") ;
+    }
+}
