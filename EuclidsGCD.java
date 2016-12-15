0b1db8df4656a03c0b0e4042119635ba6d31aa6e
Dmitri G. Brengauz <dmitri@momus.net>
2016-12-14 17:03:31 -0700

    Euclid method for greatest common denominator now uses BigInteger

---
+++ EuclidsGCD.java
@@ -0,0 +1,44 @@
+import java.math.BigInteger ; 
+
+public class EuclidsGCD {
+
+    public static void main (String[] args) {
+	BigInteger p = new BigInteger(args[0]) ;
+	BigInteger q = new BigInteger(args[1]) ;
+	BigInteger[] WhatAUselessWayToPassArgs = new BigInteger[2];
+	WhatAUselessWayToPassArgs[0] = p ;
+	WhatAUselessWayToPassArgs[1] = q ;
+
+	BigInteger[] thisGCD = gcd(WhatAUselessWayToPassArgs) ;
+	BigInteger theAnswer = thisGCD[0] ;
+	
+	System.out.print( theAnswer.toString() + "\n" ) ;
+	
+	
+    } 
+
+    
+    /**
+     * Euclid's <code>GCD</code> algorithm.
+     *
+     * @param args an <code>int</code> takes the first two ints in an
+     * array.
+     * @return an <code>int</code> the greatest common denominator.
+     */
+    private static BigInteger[]  gcd  (BigInteger[] args) {
+
+	BigInteger p = args[0] ;
+	BigInteger q = args[1] ;
+	
+	if (q.equals(BigInteger.ZERO)) {
+	    BigInteger[] FinalReturn = { p } ;
+		return FinalReturn ;
+	    }
+	else {
+	BigInteger r = p.remainder(q) ;
+	BigInteger[ ] newArgs = { q, r, } ;
+	return gcd(newArgs) ;
+	}
+    }
+
+}
--- prj.el
+++ prj.el
@@ -1,2 +1,3 @@
 (jdee-project-file-version "1.0")
 (jdee-set-variables)
+(setq jdee-server-dir ".")
