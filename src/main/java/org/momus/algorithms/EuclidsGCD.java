f75ef6a25ea882f5972acc80bcb02503e6de9b49
Dmitri G. Brengauz <dmitri@momus.net>
2016-12-17 11:09:09 -0700

    alg4 installed as a depencency.

---
+++ src/main/java/org/momus/algorithms/EuclidsGCD.java
@@ -0,0 +1,39 @@
+package org.momus.algorithms;
+
+import java.math.BigInteger;
+
+public class EuclidsGCD {
+
+    
+    public static void main (String[] args) {
+	BigInteger p = new BigInteger(args[0]);
+	BigInteger q = new BigInteger(args[1]);
+	BigInteger[] whatAUselessWayToPassArgs =  { p, q };
+	BigInteger[] thisGCD = gcd(whatAUselessWayToPassArgs);
+	BigInteger theAnswer = thisGCD[0];
+	System.out.print( theAnswer.toString() + "\n" );
+    }
+
+    
+    /**
+     * Euclid's <code>GCD</code> algorithm.
+     *
+     * @param args an <code>int</code> takes the first two ints in an
+     * array.
+     * @return an <code>int</code> the greatest common denominator.
+     */
+    private static BigInteger[]  gcd  (BigInteger[] args) {
+
+	BigInteger p = args[0];
+	BigInteger q = args[1];
+	
+	if (q.equals(BigInteger.ZERO)) {
+	    BigInteger[] FinalReturn = { p };
+		return FinalReturn; }
+	else {
+	    BigInteger r = p.remainder(q);
+	    BigInteger[ ] newArgs = {q, r };
+	    return gcd(newArgs);
+	}
+    }
+}
